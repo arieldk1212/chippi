0aee3773fd81c62906bb589417d02a0f9d914d3c
Ariel <arieldk6@gmail.com>
2024-07-30 23:33:16 +0300

    finished sdl window

--- src/display/display.h
+++ src/display/display.h
@@ -1,10 +1,34 @@
 #pragma once
+#include <iostream>
 #include <SDL2/SDL.h>
 
-void createWindow(SDL_WINDOW **window, char title[10], H, W) {
-  *gWindow = SDL_CreateWindow("Chippi-8",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              H, W,
-                              SDL_WINDOW_SHOWN);
+#define EXT_WINDOW_ERROR 0
+
+Uint32 flags = SDL_WINDOW_OPENGL;
+
+void createWindow(SDL_Window **window, char *title, const int HEIGHT, const int WIDTH, Uint32 flags) {
+
+  bool quit = false;
+  SDL_Event e;
+
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+  SDL_DisplayMode current;
+  SDL_GetCurrentDisplayMode(0, &current);  
+
+  *window = SDL_CreateWindow("Chippi-8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, HEIGHT, WIDTH, SDL_RENDERER_ACCELERATED);
+  while (!quit) {
+    if (*window == NULL) {
+      std::cerr << "Window Could Not Be Created - " << SDL_GetError() << "\n";
+      exit(EXT_WINDOW_ERROR); };
+      
+    while (SDL_PollEvent (&e) != 0) {
+      if (e.type == SDL_QUIT) {
+        quit = true;}}
+  }
+  SDL_DestroyWindow(*window);
+  SDL_Quit();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,17 @@
-#include <iostream>
-#include <display.h>
-#include <SDL2/SDL.h>
+#include "display/display.h"
 
+// Screen Settings
+char *title = "Chippi";
 const int WIDTH = 640;
 const int HEIGHT = 480;
 
+// Flags for The Screen -> in display.h
+extern Uint32 flags;
 
 int main () {
 
-  SDL_Window *gWindow;
-  createWindow(&gWindow, "Chippi", HEIGHT, WIDTH)
+  SDL_Window *window;
+  createWindow(&window, title, HEIGHT, WIDTH, flags);
 
-  // SDL_Window *window = NULL;
-  // SDL_Surface *screenSurface = NULL;
-  // SDL_Event e;
-  // bool quit = false;
-
-  // if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-  //   std::cerr << "SDL Could Not Initialize.\n";
-  // } else {
-  //   window = SDL_CreateWindow("Chippi", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-  //   if (window == NULL) {
-  //     std::cerr << "Window Could Not Be Created.\n";
-  //   } else {
-  //     screenSurface = SDL_GetWindowSurface(window);
-  //     SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-  //     SDL_UpdateWindowSurface(window);
-  //     while(quit == false) { 
-  //       while(SDL_PollEvent(&e)) {
-  //         if (e.type == SDL_QUIT) quit = true;
-  //       }
-  //     }
-  //     SDL_DestroyWindow(window);
-  //     SDL_Quit();
   return 0;
 }
